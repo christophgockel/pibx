7aeec280122b32254f934cf9dcfdc3b294296e37
Christoph Gockel <github@christophgockel.de>
2011-10-18 18:13:58 +0200

    Added new test-case for reference example "IntAttribute"

--- Tests/Scenarios/Reference/Basic/Suite.php
+++ Tests/Scenarios/Reference/Basic/Suite.php
@@ -97,6 +97,7 @@ require_once 'Tests/Scenarios/Reference/Basic/NCNameAttributeTest.php';
 require_once 'Tests/Scenarios/Reference/Basic/LongElementTest.php';
 require_once 'Tests/Scenarios/Reference/Basic/LongAttributeTest.php';
 require_once 'Tests/Scenarios/Reference/Basic/IntElementTest.php';
+require_once 'Tests/Scenarios/Reference/Basic/IntAttributeTest.php';
 /**
  * Basic Reference Test-Suite.
  *
@@ -176,6 +177,7 @@ class PiBX_Scenarios_Reference_Basic_Suite extends PHPUnit_Framework_TestSuite {
         $suite->addTestSuite('PiBX_Scenarios_Reference_Basic_LongElementTest');
         $suite->addTestSuite('PiBX_Scenarios_Reference_Basic_LongAttributeTest');
         $suite->addTestSuite('PiBX_Scenarios_Reference_Basic_IntElementTest');
+        $suite->addTestSuite('PiBX_Scenarios_Reference_Basic_IntAttributeTest');
 
         return $suite;
     }
