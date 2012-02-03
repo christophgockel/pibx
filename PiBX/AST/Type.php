d8e1c3f62941b6b9abf7007e157bccc822e71049
Christoph Gockel <github@christophgockel.de>
2012-02-03 22:42:37 +0100

    Housekeeping. Copyright year bump.

--- PiBX/AST/Type.php
+++ PiBX/AST/Type.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2010-2011, Christoph Gockel <christoph@pibx.de>.
+ * Copyright (c) 2010-2012, Christoph Gockel <christoph@pibx.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
